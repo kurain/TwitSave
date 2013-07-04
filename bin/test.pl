57b00af91ced062e332bd8dc41a3672233858d83
Ryutaro Kurai <ryu.kurai@gmail.com>
2013-07-04 17:54:54 +0900

    test update

--- bin/test.pl
+++ bin/test.pl
@@ -3,6 +3,7 @@ package TwitSave;
 use AnyEvent::Twitter::Stream;
 use Encode qw/encode_utf8 decode_utf8/;
 use Config::Pit;
+use Perl6::Say;
 
 my $config = Config::Pit::get('twitter.com');
 my $done = AE::cv;
@@ -13,11 +14,20 @@ my $listener = AnyEvent::Twitter::Stream->new(
     token_secret    => $config->{token_secret},
     method          => "sample",
     on_tweet        => sub {
-        my ($tweet) = @_;
-        if ($tweet->{lang} eq 'ja') {
-            warn $tweet->{text};
-        }
+      my ($tweet) = @_;
+      if ($tweet->{lang} eq 'ja') {
+        printf Encode::is_utf8($tweet->{text}) ? "utf8: ": "no utf8: ";
+        printf encode_utf8($tweet->{text}) . "\n";
+      }
     },
+    timeout        => 10,
+    on_connect      => sub {
+        warn 'connect';
+    },
+    on_error => sub {
+        warn $@;
+        warn 'error';
+    }
 );
 
 $self->{listener} = $listener;
--- cpanfile
+++ cpanfile
@@ -5,5 +5,5 @@ requires 'Config::Pit', 0.04;
 requires 'DBI', 1.627;
 requires 'DBD::mysql', 4.023;
 requires 'Net::OAuth', 0.28;
-
+requires 'Net::SSLeay', 1.51;
 
