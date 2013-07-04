af2578a625becb321a1f64d61903d8e3fa10b298
Ryutaro Kurai <ryu.kurai@gmail.com>
2013-07-04 15:40:22 +0900

    carton update

--- bin/run.sh
+++ bin/run.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
-APP_HOME=/home/kurain/apps/TwitSave/releases
+
+export APPROOT=/home/kurain/apps/TwitSave
+export CPANLIB=$APPROOT/shared/lib/perl5/
+export PERL5LIB=$APPROOT/shared/lib/perl5/x86_64-linux/:$APPROOT/shared/lib/perl5
+export PERLBASE=/home/kurain/perl5/perlbrew/perls/perl-5.14.2
+
 exec 2>&1
 exec \
   setuidgid kurain \
-  env - PATH="$APP_HOME:$PATH" \
-  LC_ALL=C perl $APP_HOME/bin/crawler.pl
+  LC_ALL=C $PERLBASE/bin/perl -I$APPROOT/shared/lib/perl5 -I$APPROOT/current/lib $APPROOT/current/bin/crawler.pl
 
--- cpanfile
+++ cpanfile
@@ -4,5 +4,6 @@ requires 'FindBin::libs', '1.63.0';
 requires 'Config::Pit', 0.04;
 requires 'DBI', 1.627;
 requires 'DBD::mysql', 4.023;
+requires 'Net::OAuth', 0.28;
 
 
