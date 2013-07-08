40aef8ce6336c0f592aec71d42c48236fb894982
Ryutaro Kurai <ryu.kurai@gmail.com>
2013-07-08 12:31:50 +0900

    syntax miss

--- bin/run.sh
+++ bin/run.sh
@@ -4,9 +4,10 @@ export APPROOT=/home/kurain/apps/TwitSave
 export CPANLIB=$APPROOT/shared/lib/perl5/
 export PERL5LIB=$APPROOT/shared/lib/perl5/x86_64-linux/:$APPROOT/shared/lib/perl5
 export PERLBASE=/home/kurain/perl5/perlbrew/perls/perl-5.14.2
+export LC_ALL=C
 
 exec 2>&1
 exec \
   setuidgid kurain \
-  LC_ALL=C $PERLBASE/bin/perl -I$APPROOT/shared/lib/perl5 -I$APPROOT/current/lib $APPROOT/current/bin/crawler.pl
+  $PERLBASE/bin/perl -I$APPROOT/shared/lib/perl5 -I$APPROOT/current/lib $APPROOT/current/bin/crawler.pl
 
